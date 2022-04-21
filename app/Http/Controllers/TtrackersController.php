151c2257c12f219909c72670a93fa45b8ef2a367
Mariam Ghoneem <maryam.ghoneem@yahoo.com>
2022-04-21 12:53:13 +0200

    feeding tracker

--- app/Http/Controllers/TtrackersController.php
+++ app/Http/Controllers/TtrackersController.php
@@ -13,16 +13,22 @@ class TtrackersController extends Controller
     
     public function add_feeding(Request $request, $user_id)
     {  
-        $feeding = new Feeding();
-        $feeding -> quantity    = $request->quantity;
-        $feeding -> time        = $request->time;
-        $feeding -> user_id     = $user_id;
-        $feeding -> baby_id     = $request->baby_id;
-        try {
-            $feeding->save();
-            return response()->json($feeding);
-        } catch ( Exception  $th) {
-            return response()->json(['error: no user id or baby id found']);
+        
+        $quantity = $request->quantity;
+        if (0<$quantity and $quantity<6) {
+            $feeding = new Feeding();
+            $feeding -> quantity    = $request->quantity;
+            $feeding -> time        = $request->time;
+            $feeding -> user_id     = $user_id;
+            $feeding -> baby_id     = $request->baby_id;
+            try {
+                $feeding->save();
+                return response()->json($feeding);
+            } catch ( Exception  $th) {
+                return response()->json(['error: no user id or baby id found']);
+            }
+        } else {
+            return response()->json('Quantity must be a scale from 1 to 5');
         }
         
     }
