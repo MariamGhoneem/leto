0bfd9c05e8da84a2c6da7fc41bd19d46844c043d
Mariam Ghoneem <maryam.ghoneem@yahoo.com>
2022-04-21 21:27:58 +0200

    sleep tracker

--- app/Http/Controllers/TtrackersController.php
+++ app/Http/Controllers/TtrackersController.php
@@ -4,19 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Feeding;
 use App\Models\Sleep;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 Use Exception;
-
-
 
 class TtrackersController extends Controller
 {
     
     public function add_feeding(Request $request, $user_id)
-    {  
-        
-        $quantity = $request->quantity;
-        if (0<$quantity and $quantity<6) {
+    {
+        $rules = array(
+            'quantity' => ['required',Rule::in(['1','2','3','4','5'])],
+            'time' => 'required',
+            'baby_id' => 'required',
+        );
+
+        $validated = Validator::make($request->all(),$rules);
+
+        if ($validated->fails()) {
+            return $validated->errors();
+        }
+        else{
             $feeding = new Feeding();
             $feeding -> quantity    = $request->quantity;
             $feeding -> time        = $request->time;
@@ -28,25 +37,39 @@ class TtrackersController extends Controller
             } catch ( Exception  $th) {
                 return response()->json(['error: no user id or baby id found']);
             }
-        } else {
-            return response()->json('Quantity must be a scale from 1 to 5');
         }
         
     }
 
     public function add_sleep(Request $request, $user_id)
     {
-        $sleep = new Sleep();
-        $sleep -> start_time =   $request-> start_time;
-        $sleep -> end_time =     $request-> end_time;
-        $sleep -> user_id =     $user_id;
-        $sleep -> baby_id =     $request->baby_id;
+        $rules = array(
+            'start_time' => 'required',
+            'end_time' => 'required',
+            'baby_id' => 'required',
+        );
+        $validated = Validator::make($request->all(),$rules);
 
-        try {
-            $sleep->save();
-            return response()->json($sleep);
-        } catch ( Exception  $th) {
-            return response()->json(['error: no user id or baby id found']);
+        if ($validated->fails()) {
+            return $validated->errors();
         }
+        else{
+
+            $sleep = new Sleep();
+            $sleep -> start_time =   $request->start_time;
+            $sleep -> end_time =     $request-> end_time;
+            $sleep -> user_id =      $user_id;
+            $sleep -> baby_id =      $request->baby_id;
+
+            try {
+                $sleep->save();
+                return response()->json($sleep);
+            } catch ( Exception  $th) {
+                return response()->json(['error: no user id or baby id found']);
+            }
+
+        }
+
+        
     }
 }
